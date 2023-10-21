9b54b3c91d69f217b2a828d37bc130a34fdb8f0a
Harshal Dave <dave.harshal1188@gmail.com>
2023-10-21 18:02:21 +0530

    quick sort optimization

--- sorting/quick.c
+++ sorting/quick.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-int a[] = {13,7,42,26,9,18,45,2};
-int n = sizeof(a)/sizeof(a[0]);
+int a[] = {13, 7, 42, 26, 9, 18, 45, 2};
+int n = sizeof(a) / sizeof(a[0]);
 
 void quick_sort(int low, int high);
 int partition(int low, int high);
@@ -9,52 +11,60 @@ void swap(int i, int j);
 
 int main(int argc, char const *argv[])
 {
-    
-    quick_sort(0,n-1);
+
+    quick_sort(0, n - 1);
     printf("Sorted List:\n");
     for (int i = 0; i < n; i++)
-        printf("%d,",a[i]);
+        printf("%d,", a[i]);
     printf("\n");
     return 0;
 }
-void quick_sort(int low, int high){
+void quick_sort(int low, int high)
+{
     int m;
-    if(low < high){    
-        m = partition(low,high);
-        quick_sort(low,m-1);
-        quick_sort(m+1,high);
+    if (low < high)
+    {
+        m = partition(low, high);
+        quick_sort(low, m - 1);
+        quick_sort(m + 1, high);
     }
 }
-int partition(int low, int high){
+int partition(int low, int high)
+{
+    srand(time(NULL));
+
+    int random = low + rand() % (high - low + 1);
+    swap(low, random);
     int i = low, j = high, pivot = a[low];
     while (i < j)
     {
         // Find larger elment than pivot
-        while(pivot >= a[i] && i <= j)
+        while (pivot >= a[i] && i <= j)
             i++;
 
         // Find Smaller element than pivot
-        while(pivot < a[j])
+        while (pivot < a[j])
             j--;
         // If i is smaller than j that means there's an a[i] which is larger than a[j] so swap them.
-        if(i < j)
-            swap(i,j);
+        if (i < j)
+            swap(i, j);
     }
     // j will surpass i and we will find pivot's correct position so put it there.
-    swap(low,j);
-    
-    //-------------Tracing------------------------------------: 
-    for(int k = low; k <= high; k++)
-        printf("%d,",a[k]);
+    swap(low, j);
+
+    //-------------Tracing------------------------------------:
+    for (int k = low; k <= high; k++)
+        printf("%d,", a[k]);
     printf("\n");
-    printf("%d was placed in its correct position.\n\n",a[j]);
+    printf("%d was placed in its correct position.\n\n", a[j]);
     //---------------------------------------------------------
 
     return j;
 }
-void swap(int i, int j){
+void swap(int i, int j)
+{
     int temp;
     temp = a[i];
     a[i] = a[j];
     a[j] = temp;
-}   
+}
